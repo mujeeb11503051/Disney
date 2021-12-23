5def47e0457a1889e8af32453be6dfb32c619c97
mujeeb11503051 <mujeebmjrahman@gmail.com>
2021-12-23 18:03:56 +0100

    set up and added firebase to the project

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBrvE9_YQDhTnbz7JWt8lWuMVpzHxAviwg",
+    authDomain: "disneyplus-clone-cca18.firebaseapp.com",
+    projectId: "disneyplus-clone-cca18",
+    storageBucket: "disneyplus-clone-cca18.appspot.com",
+    messagingSenderId: "509367218199",
+    appId: "1:509367218199:web:be61b113a33aa3e2a1ac52"
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const storage = firebase.storage();
+  
+  export { auth, provider, storage };
+  export default db;
